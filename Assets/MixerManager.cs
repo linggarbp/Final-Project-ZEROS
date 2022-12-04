d8f0244cbafbd948829c0a4985683251c3ab2ec3
RikiAndriana <rikiandrianakuliah@gmail.com>
2022-12-04 22:05:11 +0700

    Game Over Panel & Audio Mixer

--- Assets/MixerManager.cs
+++ Assets/MixerManager.cs
@@ -2,19 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MixerManager : MonoBehaviour
 {
     [SerializeField] AudioMixer audioMixer;
-    [SerializeField, Range(0, 1f)] float volumeMaster = 1;
-    [SerializeField, Range(0, 1f)] float volumeBGM = 1;
-    [SerializeField, Range(0, 1f)] float volumeSFX = 1;
-    private void Update()
+    // [SerializeField, Range(0, 1f)] float volumeMaster = 1;
+    // [SerializeField, Range(0, 1f)] float volumeBGM = 1;
+    // [SerializeField, Range(0, 1f)] float volumeSFX = 1;
+
+
+    private void Awake()
     {
-        audioMixer.SetFloat("Mixer_Master", LinearToDecibel(volumeMaster));
-        audioMixer.SetFloat("Mixer_BGM", LinearToDecibel(volumeBGM));
-        audioMixer.SetFloat("Mixer_SFX", LinearToDecibel(volumeSFX));
+        audioMixer.SetFloat("Mixer_Master", PlayerPrefs.GetFloat("MasterVolume", 0.8f));
+        audioMixer.SetFloat("Mixer_BGM", PlayerPrefs.GetFloat("MusicVolume", 0.8f));
+        audioMixer.SetFloat("Mixer_SFX", PlayerPrefs.GetFloat("SFXVolume", 0.8f));
     }
+
     private float LinearToDecibel(float linear)
     {
         // linear = linear < 0.0001f?0.0001f:linear;
